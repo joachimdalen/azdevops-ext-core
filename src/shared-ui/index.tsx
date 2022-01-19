4ce0713ee01b111a55877fa24419b2fd84c67eea
Joachim Dalen <me@d3v.no>
2022-01-19 20:09:17 +0100

    feat: Add ExtendedZeroData

--- src/shared-ui/index.tsx
+++ src/shared-ui/index.tsx
@@ -1,5 +1,6 @@
 export { default as LoadingSection } from './component/LoadingSection';
 export { default as VersionDisplay } from './component/VersionDisplay';
+export { default as ExtendedZeroData } from './component/ExtendedZeroData';
 export { default as useDropdownSelection } from './hooks/useDropdownSelection';
 export { default as useResizeTimeout } from './hooks/useResizeTimeout';
 export { default as PanelWrapper } from './wrappers/PanelWrapper';
