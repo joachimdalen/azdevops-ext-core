d8551a98172a26bf495d12d31465a32610f366d5
Joachim Dalen <me@d3v.no>
2022-02-13 10:43:42 +0100

    fix(ExtendedZeroData): Fixed parameter issues

--- src/shared-ui/component/ExtendedZeroData.tsx
+++ src/shared-ui/component/ExtendedZeroData.tsx
@@ -3,14 +3,22 @@ import { ButtonGroup } from 'azure-devops-ui/ButtonGroup';
 import { Icon, IIconProps } from 'azure-devops-ui/Icon';
 
 interface ExtendedZeroDataProps {
+  title: string;
+  description?: string;
   buttons: IButtonProps[];
   icon?: IIconProps;
 }
-const ExtendedZeroData = ({ buttons, icon }: ExtendedZeroDataProps): JSX.Element => {
+const ExtendedZeroData = ({
+  buttons,
+  icon,
+  title,
+  description
+}: ExtendedZeroDataProps): JSX.Element => {
   return (
     <div className="flex-column flex-center margin-vertical-16">
       {icon && <Icon className="custom-zero-data-icon" {...icon} />}
-      <div className="margin-horizontal-16 title-l">No rules added</div>
+      <div className="margin-horizontal-16 title-l">{title}</div>
+      {description && <div className="margin-top-16">{description}</div>}
       <ButtonGroup className="margin-top-16">
         {buttons.map((button, index) => (
           <Button key={button.id || `button-${index}`} id={button.id} {...button} />
